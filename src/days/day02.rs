9108e9133d9b75bce0dcf86924bbbcbc3ca4e66b
acikek <spiralixp@gmail.com>
2021-12-02 09:35:14 -0800

    day 2

---
+++ src/days/day02.rs
@@ -0,0 +1,43 @@
+use crate::solution;
+use crate::util::lines;
+
+pub fn first_second(lines: &Vec<String>) -> (isize, isize) {
+    // Problem 1
+    let mut x = 0;
+    let mut y1 = 0;
+
+    // Problem 2
+    let mut y2 = 0;
+    let mut aim = 0;
+
+    for line in lines {
+        let pair = line.split(" ").collect::<Vec<&str>>();
+
+        let cmd = pair[0];
+        let n = pair[1].parse::<isize>().unwrap();
+
+        match cmd {
+            "forward" => { 
+                x += n;
+                y2 += aim * n;
+            }
+            "down" => { 
+                y1 += n;
+                aim += n;
+            }
+            "up" => {
+                y1 -= n;
+                aim -= n;
+            }
+            _ => unimplemented!()
+        }
+    }
+
+    (x * y1, x * y2)
+}
+
+pub fn exec() {
+    let values = first_second(&lines());
+    solution!(1: values.0);
+    solution!(2: values.1);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+#![allow(dead_code)]
+
 mod days;
 mod util;
 
 // Change the number to the day you desire
-use days::day01::exec;
+use days::day02::exec;
 
 fn main() {
     exec()
